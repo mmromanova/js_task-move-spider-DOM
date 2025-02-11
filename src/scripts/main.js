f7db350ccd46d74d3725647740aca4cb5b4146e7
mmromanova <emy2004.bo@gmail.com>
2025-02-11 17:38:07 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,28 @@
-'use strict';
-
 document.addEventListener('click', (ev) => {
   const spider = document.querySelector('.spider');
   const wall = document.querySelector('.wall');
 
+  // Get the dimensions of the wall
   const wallRect = wall.getBoundingClientRect();
-  const spiderRect = spider.getBoundingClientRect();
 
-  let newLeft = ev.clientX - wallRect.left - spider.clientWidth / 2;
-  let newTop = ev.clientY - wallRect.top - spider.clientHeight / 2;
+  // Check if the click is inside the wall
+  const isInsideWall =
+    ev.clientX >= wallRect.left &&
+    ev.clientX <= wallRect.right &&
+    ev.clientY >= wallRect.top &&
+    ev.clientY <= wallRect.bottom;
+  // ev.clientY + spider.clientHeight + 20 >= wallRect.bottom &&
+  // ev.clientX + spider.clientWidth + 20 >= wallRect.right &&
+  // ev.clientY - spider.clientHeigth - 10 <= wallRect.top &&
+  // ev.clientX - spider.clientWidth - 10 <= wallRect.left;
 
-  if (newTop < 0) {
-    newTop = 0;
-  } else if (newTop + spider.clientHeight + 20 > wallRect.height) {
-    newTop = wallRect.height - spider.clientHeight - 20;
+  if (isInsideWall) {
+    // Calculate the new position of the spider
+    const newLeft = ev.clientX - wallRect.left - spider.clientWidth / 2;
+    const newTop = ev.clientY - wallRect.top - spider.clientHeight / 2;
+
+    // Apply the calculated positions to the spider
+    spider.style.left = `${newLeft}px`;
+    spider.style.top = `${newTop}px`;
   }
-
-  if (newLeft < 0) {
-    newLeft = 0;
-  } else if (newLeft + spider.clientWidth + 20 > wallRect.width) {
-    newLeft = wallRect.width - spider.clientWidth - 20;
-  }
-
-  spider.style.left = `${newLeft}px`;
-  spider.style.top = `${newTop}px`;
 });
